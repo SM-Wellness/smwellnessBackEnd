3185d973089ab9fdcb799aa1fc31789aba4a6cdc
csyeob <csyeob@gmail.com>
2023-02-04 20:24:36 +0900

    port number change

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ import documentBuilder from '@app/config/document-builder';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  const port = process.env.PORT || 3000
+  const port = process.env.PORT || 8080
   app.enableCors();
 
   documentBuilder({
